b78d784d85c3d395946bf813a64d3a54bd37e5c4
William Bushie <willbushie@gmail.com>
2023-12-04 23:48:22 -0600

    resolve `hasPermission()` error in `Networking.tsx`

--- Handlers/Networking.tsx
+++ Handlers/Networking.tsx
@@ -4,8 +4,10 @@ import {
     PermissionsAndroid,
     Platform,
     ToastAndroid,
+    Linking,
 } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
+import appConfig from '../app.json';
 
 
 
@@ -274,6 +276,43 @@ const hasLocationPermission = async () => {
             'Location permission revoked by user.',
             ToastAndroid.LONG,
         );
+    }
+
+    return false;
+};
+
+/**
+ * Check for user permission on IOS.
+ * 
+ * reference: https://github.com/Agontuk/react-native-geolocation-service/blob/master/example/src/App.tsx
+ * 
+ * @returns boolean
+ */
+const hasPermissionIOS = async () => {
+    const openSetting = () => {
+      Linking.openSettings().catch(() => {
+        Alert.alert('Unable to open settings');
+      });
+    };
+    const status = await Geolocation.requestAuthorization('whenInUse');
+
+    if (status === 'granted') {
+      return true;
+    }
+
+    if (status === 'denied') {
+      Alert.alert('Location permission denied');
+    }
+
+    if (status === 'disabled') {
+      Alert.alert(
+        `Turn on Location Services to allow "${appConfig.displayName}" to determine your location.`,
+        '',
+        [
+          { text: 'Go to Settings', onPress: openSetting },
+          { text: "Don't Use Location", onPress: () => {} },
+        ],
+      );
     }
 
     return false;
